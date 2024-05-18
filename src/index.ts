c4e0d3ae3d59833a76772affb249ab9ec1c20d42
Adam <adam@haglund.dev>
2024-05-18 21:21:53 +0200

    formatting

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,53 @@
 // Helpers.
-const s = 1000;
-const m = s * 60;
-const h = m * 60;
-const d = h * 24;
-const w = d * 7;
-const y = d * 365.25;
+const s = 1000
+const m = s * 60
+const h = m * 60
+const d = h * 24
+const w = d * 7
+const y = d * 365.25
 
 type Unit =
-  | 'Years'
-  | 'Year'
-  | 'Yrs'
-  | 'Yr'
-  | 'Y'
-  | 'Weeks'
-  | 'Week'
-  | 'W'
-  | 'Days'
-  | 'Day'
-  | 'D'
-  | 'Hours'
-  | 'Hour'
-  | 'Hrs'
-  | 'Hr'
-  | 'H'
-  | 'Minutes'
-  | 'Minute'
-  | 'Mins'
-  | 'Min'
-  | 'M'
-  | 'Seconds'
-  | 'Second'
-  | 'Secs'
-  | 'Sec'
-  | 's'
-  | 'Milliseconds'
-  | 'Millisecond'
-  | 'Msecs'
-  | 'Msec'
-  | 'Ms';
+  | "Years"
+  | "Year"
+  | "Yrs"
+  | "Yr"
+  | "Y"
+  | "Weeks"
+  | "Week"
+  | "W"
+  | "Days"
+  | "Day"
+  | "D"
+  | "Hours"
+  | "Hour"
+  | "Hrs"
+  | "Hr"
+  | "H"
+  | "Minutes"
+  | "Minute"
+  | "Mins"
+  | "Min"
+  | "M"
+  | "Seconds"
+  | "Second"
+  | "Secs"
+  | "Sec"
+  | "s"
+  | "Milliseconds"
+  | "Millisecond"
+  | "Msecs"
+  | "Msec"
+  | "Ms"
 
-type UnitAnyCase = Unit | Uppercase<Unit> | Lowercase<Unit>;
+type UnitAnyCase = Unit | Uppercase<Unit> | Lowercase<Unit>
 
-type StringValue =
-  | `${number}`
-  | `${number}${UnitAnyCase}`
-  | `${number} ${UnitAnyCase}`;
+type StringValue = `${number}` | `${number}${UnitAnyCase}` | `${number} ${UnitAnyCase}`
 
 interface Options {
   /**
    * Set to `true` to use verbose formatting. Defaults to `false`.
    */
-  long?: boolean;
+  long?: boolean
 }
 
 /**
@@ -60,21 +57,21 @@ interface Options {
  * @param options - Options for the conversion
  * @throws Error if `value` is not a non-empty string or a number
  */
-function ms(value: StringValue, options?: Options): number;
-function ms(value: number, options?: Options): string;
+function ms(value: StringValue, options?: Options): number
+function ms(value: number, options?: Options): string
 function ms(value: StringValue | number, options?: Options): number | string {
   try {
-    if (typeof value === 'string') {
-      return parse(value);
-    } else if (typeof value === 'number') {
-      return format(value, options);
+    if (typeof value === "string") {
+      return parse(value)
+    } else if (typeof value === "number") {
+      return format(value, options)
     }
-    throw new Error('Value provided to ms() must be a string or number.');
+    throw new Error("Value provided to ms() must be a string or number.")
   } catch (error) {
     const message = isError(error)
       ? `${error.message}. value=${JSON.stringify(value)}`
-      : 'An unknown error has occurred.';
-    throw new Error(message);
+      : "An unknown error has occurred."
+    throw new Error(message)
   }
 }
 
@@ -86,67 +83,67 @@ function ms(value: StringValue | number, options?: Options): number | string {
  * parsed
  */
 function parse(str: string): number {
-  if (typeof str !== 'string' || str.length === 0 || str.length > 100) {
+  if (typeof str !== "string" || str.length === 0 || str.length > 100) {
     throw new Error(
-      'Value provided to ms.parse() must be a string with length between 1 and 99.',
-    );
+      "Value provided to ms.parse() must be a string with length between 1 and 99.",
+    )
   }
   const match =
     /^(?<value>-?(?:\d+(?:\.\d+)?|\.\d+)) *(?<type>milliseconds?|msecs?|ms|seconds?|secs?|s|minutes?|mins?|m|hours?|hrs?|h|days?|d|weeks?|w|years?|yrs?|y)?$/i.exec(
       str,
-    );
+    )
   // Named capture groups need to be manually typed today.
   // https://github.com/microsoft/TypeScript/issues/32098
-  const groups = match?.groups as { value: string; type?: string } | undefined;
+  const groups = match?.groups as { value: string; type?: string } | undefined
   if (!groups) {
-    return Number.NaN;
+    return Number.NaN
   }
-  const n = Number.parseFloat(groups.value);
-  const type = (groups.type || 'ms').toLowerCase() as Lowercase<Unit>;
+  const n = Number.parseFloat(groups.value)
+  const type = (groups.type || "ms").toLowerCase() as Lowercase<Unit>
   switch (type) {
-    case 'years':
-    case 'year':
-    case 'yrs':
-    case 'yr':
-    case 'y':
-      return n * y;
-    case 'weeks':
-    case 'week':
-    case 'w':
-      return n * w;
-    case 'days':
-    case 'day':
-    case 'd':
-      return n * d;
-    case 'hours':
-    case 'hour':
-    case 'hrs':
-    case 'hr':
-    case 'h':
-      return n * h;
-    case 'minutes':
-    case 'minute':
-    case 'mins':
-    case 'min':
-    case 'm':
-      return n * m;
-    case 'seconds':
-    case 'second':
-    case 'secs':
-    case 'sec':
-    case 's':
-      return n * s;
-    case 'milliseconds':
-    case 'millisecond':
-    case 'msecs':
-    case 'msec':
-    case 'ms':
-      return n;
+    case "years":
+    case "year":
+    case "yrs":
+    case "yr":
+    case "y":
+      return n * y
+    case "weeks":
+    case "week":
+    case "w":
+      return n * w
+    case "days":
+    case "day":
+    case "d":
+      return n * d
+    case "hours":
+    case "hour":
+    case "hrs":
+    case "hr":
+    case "h":
+      return n * h
+    case "minutes":
+    case "minute":
+    case "mins":
+    case "min":
+    case "m":
+      return n * m
+    case "seconds":
+    case "second":
+    case "secs":
+    case "sec":
+    case "s":
+      return n * s
+    case "milliseconds":
+    case "millisecond":
+    case "msecs":
+    case "msec":
+    case "ms":
+      return n
     default:
       // This should never occur.
       throw new Error(
         `The unit ${type as string} was matched, but no matching case exists.`,
-      );
+      )
   }
 }
 
@@ -158,46 +155,46 @@ function parse(str: string): number {
  * parsed
  */
 function parseStrict(value: StringValue): number {
-  return parse(value);
+  return parse(value)
 }
 /**
  * Short format for `ms`.
  */
 function fmtShort(ms: number): StringValue {
-  const msAbs = Math.abs(ms);
+  const msAbs = Math.abs(ms)
   if (msAbs >= d) {
-    return `${Math.round(ms / d)}d`;
+    return `${Math.round(ms / d)}d`
   }
   if (msAbs >= h) {
-    return `${Math.round(ms / h)}h`;
+    return `${Math.round(ms / h)}h`
   }
   if (msAbs >= m) {
-    return `${Math.round(ms / m)}m`;
+    return `${Math.round(ms / m)}m`
   }
   if (msAbs >= s) {
-    return `${Math.round(ms / s)}s`;
+    return `${Math.round(ms / s)}s`
   }
-  return `${ms}ms`;
+  return `${ms}ms`
 }
 
 /**
  * Long format for `ms`.
  */
 function fmtLong(ms: number): StringValue {
-  const msAbs = Math.abs(ms);
+  const msAbs = Math.abs(ms)
   if (msAbs >= d) {
-    return plural(ms, msAbs, d, 'day');
+    return plural(ms, msAbs, d, "day")
   }
   if (msAbs >= h) {
-    return plural(ms, msAbs, h, 'hour');
+    return plural(ms, msAbs, h, "hour")
   }
   if (msAbs >= m) {
-    return plural(ms, msAbs, m, 'minute');
+    return plural(ms, msAbs, m, "minute")
   }
   if (msAbs >= s) {
-    return plural(ms, msAbs, s, 'second');
+    return plural(ms, msAbs, s, "second")
   }
-  return `${ms} ms`;
+  return `${ms} ms`
 }
 
 /**
@@ -208,23 +205,18 @@ function fmtLong(ms: number): StringValue {
  * @returns The formatted string
  */
 function format(ms: number, options?: Options): string {
-  if (typeof ms !== 'number' || !Number.isFinite(ms)) {
-    throw new TypeError('Value provided to ms.format() must be of type number.');
+  if (typeof ms !== "number" || !Number.isFinite(ms)) {
+    throw new TypeError("Value provided to ms.format() must be of type number.")
   }
-  return options?.long ? fmtLong(ms) : fmtShort(ms);
+  return options?.long ? fmtLong(ms) : fmtShort(ms)
 }
 
 /**
  * Pluralization helper.
  */
-function plural(
-  ms: number,
-  msAbs: number,
-  n: number,
-  name: string,
-): StringValue {
-  const isPlural = msAbs >= n * 1.5;
-  return `${Math.round(ms / n)} ${name}${isPlural ? 's' : ''}` as StringValue;
+function plural(ms: number, msAbs: number, n: number, name: string): StringValue {
+  const isPlural = msAbs >= n * 1.5
+  return `${Math.round(ms / n)} ${name}${isPlural ? "s" : ""}` as StringValue
 }
 
 /**
@@ -234,15 +226,7 @@ function plural(
  * @returns A boolean `true` if the provided value is an Error-like object
  */
 function isError(value: unknown): value is Error {
-  return typeof value === 'object' && value !== null && 'message' in value;
+  return typeof value === "object" && value !== null && "message" in value
 }
 
-
-export {
-  type StringValue,
-
-  ms,
-  parse,
-  parseStrict,
-  format,
-}
+export { type StringValue, ms, parse, parseStrict, format }
